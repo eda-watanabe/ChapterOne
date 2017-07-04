06f10f32d3f069c205c397ac1e210c18f21b5acd
小林良 <kobayashi@eda-inc.jp>
2017-07-04 19:09:19 +0900

    TODO作成画面

--- ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/todocreate/TodoCreateActivity.java
+++ ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/todocreate/TodoCreateActivity.java
@@ -1,19 +1,24 @@
 package jp.inc.eda.chapterone_master.presentation.todocreate;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import jp.inc.eda.chapterone_master.R;
+import jp.inc.eda.chapterone_master.domain.model.Todo;
 import jp.inc.eda.chapterone_master.helper.Provider;
 import jp.inc.eda.chapterone_master.domain.model.OrmaDatabase;
 import jp.inc.eda.chapterone_master.infra.dao.TodoDao;
 import jp.inc.eda.chapterone_master.infra.repository.TodoRepository;
 
 public class TodoCreateActivity extends AppCompatActivity {
+
+    private TodoRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +31,26 @@ public class TodoCreateActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                // TODOを保存
+                Todo todo = new Todo();
+                todo.title = getViewText(R.id.title);
+                todo.description = getViewText(R.id.description);
+                todo.isChecked = false;
+                repository.create(todo);
+                Snackbar.make(view, "Created Todo", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
+        // DB取得
         Provider module = new Provider();
         OrmaDatabase orma = module.provideOrma(this);
+        // TODO用のDAO生成
         TodoDao dao = new TodoDao(orma);
-        TodoRepository repository = new TodoRepository(dao);
+        repository = new TodoRepository(dao);
     }
 
+    private String getViewText(@IdRes int id) {
+        return ((TextView)findViewById(id)).getText().toString();
+    }
 }
