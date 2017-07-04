99ec02ddb48106bdee1d10d0c23228f6ee51ca3a
eda watanabe <watanabe@eda-inc.jp>
2017-07-04 13:32:24 +0900

    add splash time

--- ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/launcher/LauncherActivity.java
+++ ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/launcher/LauncherActivity.java
@@ -1,12 +1,18 @@
 package jp.inc.eda.chapterone_master.presentation.launcher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.widget.TextView;
 
 import jp.inc.eda.chapterone_master.R;
+import jp.inc.eda.chapterone_master.presentation.todolist.TodoListActivity;
 
-public class LauncherActivity extends AppCompatActivity implements LauncherPresenter.Contract{
+public class LauncherActivity extends AppCompatActivity implements LauncherPresenter.Contract {
+
+    private final static int SPLASH_TIME = 1500;
 
     private LauncherPresenter presenter;
 
@@ -25,6 +31,23 @@ public class LauncherActivity extends AppCompatActivity implements LauncherPrese
     protected void onResume() {
         super.onResume();
         presenter.resume();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LauncherActivity.this, TodoListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
