303221c71a609cdf034e5d4671e6206a57b4d2a0
小林良 <kobayashi@eda-inc.jp>
2017-07-05 10:26:58 +0900

    Activity・Presenter修正

--- ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/todocreate/TodoCreateActivity.java
+++ ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/todocreate/TodoCreateActivity.java
@@ -1,11 +1,20 @@
 package jp.inc.eda.chapterone_master.presentation.todocreate;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import jp.inc.eda.chapterone_master.R;
+import jp.inc.eda.chapterone_master.domain.model.OrmaDatabase;
 import jp.inc.eda.chapterone_master.domain.model.Todo;
+import jp.inc.eda.chapterone_master.helper.Provider;
+import jp.inc.eda.chapterone_master.infra.dao.TodoDao;
+import jp.inc.eda.chapterone_master.infra.repository.TodoRepository;
 
 public class TodoCreateActivity extends AppCompatActivity implements TodoCreatePresenter.Contract {
 
@@ -18,11 +27,42 @@ public class TodoCreateActivity extends AppCompatActivity implements TodoCreateP
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        presenter = new TodoCreatePresenter(this, this);
+        // TODOリポジトリ作成
+        Provider module = new Provider();
+        OrmaDatabase orma = module.provideOrma(this);
+        // TODO用のDAO生成
+        TodoDao dao = new TodoDao(orma);
+        TodoRepository repo = new TodoRepository(dao);
+
+        // Presenter作成
+        presenter = new TodoCreatePresenter(this, repo, this);
+
+        // TODO作成ボタンコールバック設定
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = getViewText(R.id.title);
+                if(title.isEmpty()) {
+                    // タイトルが入力されていない
+                    Snackbar.make(v, "Please enter Todo title.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                    return;
+                }
+                String description = getViewText(R.id.description);
+                if(description.isEmpty()) {
+                    // 内容が入力されていない
+                    Snackbar.make(v, "Please enter Todo description.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                    return;
+                }
+                presenter.createTodo(title, description);
+                finish();
+            }
+        });
     }
 
-    @Override
-    public void onTodoCreated(Todo todo) {
-        finish();
+    private String getViewText(@IdRes int id) {
+        return ((TextView)findViewById(id)).getText().toString();
     }
 }
--- ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/todocreate/TodoCreatePresenter.java
+++ ChapterOne-master/app/src/main/java/jp/inc/eda/chapterone_master/presentation/todocreate/TodoCreatePresenter.java
@@ -23,18 +23,13 @@ import jp.inc.eda.chapterone_master.infra.repository.TodoRepository;
 class TodoCreatePresenter {
 
     interface Contract {
-        /**
-         * TODOの作成が完了した
-         * @param todo 作成したTODO
-         */
-        void onTodoCreated(Todo todo);
     }
 
     private AppCompatActivity activity;
     private TodoRepository repository;
     private Contract contract;
 
-    TodoCreatePresenter(AppCompatActivity activity, Contract _contract) {
+    TodoCreatePresenter(AppCompatActivity activity, TodoRepository repository, Contract _contract) {
         this.activity = activity;
 
         // DB取得
@@ -42,40 +37,16 @@ class TodoCreatePresenter {
         OrmaDatabase orma = module.provideOrma(activity);
         // TODO用のDAO生成
         TodoDao dao = new TodoDao(orma);
-        repository = new TodoRepository(dao);
+        this.repository = new TodoRepository(dao);
 
         this.contract = _contract;
-
-        FloatingActionButton fab = (FloatingActionButton) activity.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String title = getViewText(R.id.title);
-                if(title.isEmpty()) {
-                    // タイトルが入力されていない
-                    Snackbar.make(v, "Please enter Todo title.", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                    return;
-                }
-                String description = getViewText(R.id.description);
-                if(description.isEmpty()) {
-                    // 内容が入力されていない
-                    Snackbar.make(v, "Please enter Todo description.", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                    return;
-                }
-                // TODOを保存
-                Todo todo = new Todo();
-                todo.title = title;
-                todo.description = description;
-                todo.isChecked = false;
-                repository.create(todo);
-                contract.onTodoCreated(todo);
-            }
-        });
     }
 
-    private String getViewText(@IdRes int id) {
-        return ((TextView)activity.findViewById(id)).getText().toString();
+    void createTodo(String title, String description) {
+        Todo todo = new Todo();
+        todo.title = title;
+        todo.description = description;
+        todo.isChecked = false;
+        repository.create(todo);
     }
 }
